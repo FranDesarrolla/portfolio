fffe5c6a94e136ee08cd960ca87c9197355d1db6
FranDesarrolla <franciscovargascontacto@gmail.com>
2021-02-09 01:34:17 -0300

    Correcciones simulador

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,27 @@
 
+	function tipoPagina(){
+
+		var pagina = [
+
+		  this.tipo = tipo,
+		  this.precio = precio,
+
+		]
+
+		const Paginas = [
+
+		new pagina("sencilla",1000),
+		new pagina("elaborada",2500),
+
+		]
+	}
+
+	function calcularPrecio(){
+
+		var calcPagina = parseInt(document.getElementById("inputPag").value)
 	
+		var costo = calcPagina
+
+		var precio = document.getElementById("toma-tu-precio");
+		precio.innerText = costo;
+	}
