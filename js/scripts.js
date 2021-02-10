cdb3c6de82a79ae5c4599d6a6f0c77d566f445b5
FranDesarrolla <franciscovargascontacto@gmail.com>
2021-02-10 14:25:23 -0300

    Cambio al Simulador, quedo mejor hecho

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,88 @@
 
+	// Guardar el ID en el LocalStorage //
+
+	function userID(){
+
+		let nombre = document.getElementById("nombre-id").value;
+
+		localStorage.setItem("Nombre", nombre);
+
+		var namae = localStorage.getItem("Nombre");
+
+		console.log(namae);
+	
+	}
+
+	// Calculos de presupuesto //
+
 	function tipoPagina(){
 
-		var pagina = [
+		var paginaSen = {
+		  tipo: "Sencilla",
+		  precio: 1000,
+		  descripcion: "Pagina para emprendimientos pequeños o nuevos",
+		};
 
-		  this.tipo = tipo,
-		  this.precio = precio,
+		var paginaEla = {
+		  tipo: "Elaborada",
+		  precio: 2500,
+		  descripcion: "Pagina para emprendimientos grandes y medianos",
+		};
 
-		]
+		var tipoSelect = document.getElementById("inputPag");
+		tipoSelect.addEventListener("change", ()=> {
+			console.log("Ha cambiado su seleccion de Pagina")
+		})
 
-		const Paginas = [
+		document.getElementById("inputPag").value;
 
-		new pagina("sencilla",1000),
-		new pagina("elaborada",2500),
+	}
 
-		]
+	function tipoPlan(){
+
+		var planMen = {
+		  tipo: "Mensual",
+		  precio: 2000,
+		  descripcion: "Plan de hosting que se renueva mensualmente, ideal para proyectos chicos con pocos recursos",
+		};
+
+		var planAnual = {
+		  tipo: "Anual",
+		  precio: 10000,
+		  descripcion: "Plan de hosting anual, la mejor opcion para todos",
+		};
+
+		var tipoSelect = document.getElementById("inputPag2");
+		tipoSelect.addEventListener("change", ()=> {
+			console.log("Ha cambiado su seleccion de Plan")
+		})
+
+		document.getElementById("inputPag2").value;
+
 	}
 
 	function calcularPrecio(){
 
 		var calcPagina = parseInt(document.getElementById("inputPag").value)
-	
-		var costo = calcPagina
+		var calcPlan = parseInt(document.getElementById("inputPag2").value)
+		
+		var costo = calcPagina + calcPlan;
 
 		var precio = document.getElementById("toma-tu-precio");
 		precio.innerText = costo;
+
 	}
+
+	// Boton //
+
+	let boton = document.getElementById("boton");
+
+	boton.addEventListener("click", ()=> {
+		userID();
+		calcularPrecio();
+	});
+
+	// Funciones inicializadas //
+
+		tipoPagina();
+		tipoPlan();
